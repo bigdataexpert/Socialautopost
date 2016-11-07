a98a48a5491ef4354bb569cbbee29c32fc334546
bigdataexpert <vikash.hadoop@gmail.com>
2016-11-07 13:08:45 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+//mysql_connect('localhost','snapdeal_train','himanshu');
+//mysql_select_db('snapdeal_train');
+mysql_connect('localhost','kumbaya1_dbuser','Fsrh59?7');
+mysql_select_db('kumbaya1_db');
+define('DOMAIN_NAME', 'http://www.railroutes.in');
+
+error_reporting(E_ALL);
+
+error_reporting(1);
+ini_set('display_errors', 1);
+?>
